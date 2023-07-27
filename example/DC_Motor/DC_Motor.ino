be7016cc03dc3bc25db6260ac34ee7231b555a78
nulljun <nulljun@null-lab.com>
2023-07-27 20:00:03 +0800

    Fix dc test demo
    Fix dc test demo

--- example/DC_Motor/DC_Motor.ino
+++ example/DC_Motor/DC_Motor.ino
@@ -4,34 +4,34 @@
 #define PWM2 6  // define pwm2 6 port
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("Starting......");
   pinMode(DIR1, OUTPUT);
   pinMode(PWM1, OUTPUT);
   pinMode(DIR2, OUTPUT);
   pinMode(PWM2, OUTPUT);
-  Serial.println("Starting......");
+
 }
 
 void loop() {
   // forward speed 255
   Serial.println("M1,M2 Forward");
+  
   digitalWrite(DIR1, HIGH);
-  digitalWrite(PWM1, 255);
+  analogWrite(PWM1, 255);
   digitalWrite(DIR2, HIGH);
-  digitalWrite(PWM2, 255);
-  delay(2000);
+  analogWrite(PWM2, 255);
+  delay(1000);
   // reversal speed 255
   Serial.println("M1,M2 Reversal");
   digitalWrite(DIR1, LOW);
-  digitalWrite(PWM1, 255);
+  analogWrite(PWM1, 255);
   digitalWrite(DIR2, LOW);
-  digitalWrite(PWM2, 255);
-  delay(2000);
+  analogWrite(PWM2, 255);
+  delay(1000);
   // Stop speed 255
   Serial.println("M1,M2 Stop");
-  digitalWrite(DIR1, LOW);
-  digitalWrite(PWM1, LOW);
-  digitalWrite(DIR2, LOW);
-  digitalWrite(PWM2, LOW);
-  delay(2000);
+  analogWrite(PWM1, 0);
+  analogWrite(PWM2, 0);
+  delay(1000); 
 }
